a39157dedd357efd886cee7a4e7b61f9d061d37b
Ruusa Bolton <ruusabolton@Ruusas-MacBook-Pro.local>
2018-06-21 14:35:36 -0500

    adding defuse-the-bom

--- js/circle.js
+++ js/circle.js
@@ -2,12 +2,13 @@
     "use strict";
 
     // create a circle object
+    //START OF OBJECT
     var circle = {
         radius: 3,
 
         getArea: function () {
             // TODO: complete this method
-            return (Math.PI * Math.pow(this.radius, 2));
+            return (Math.PI * Math.pow(this.radius, 2));// TO TEST: console.log(circle.getArea()) OUTSIDE of the object!!
              // TODO: return the proper value //I combined the 2 instructions.
                             },
         logInfo: function (doRounding) {
@@ -22,10 +23,11 @@
             else{
                 res=this.getArea();
             }
-
+//OR TERNARY OPERATOR:
+// ?var res = (doRounding) ? Math.round(this.getArea()) : this.getArea();
             console.log("Area of a circle with radius of " + this.radius + " is "  +  res); //will print rounded values.
                                         }
-                };
+                }; //END OF OBJECT
 
     // log info about the circle
     circle.logInfo(false);//this should pull info from function, if not rounded.
@@ -36,4 +38,8 @@
 circle.radius = 5;
     circle.logInfo(false);//when radius is changed to 5, function will adjust the calculations and do the rounding or not.
     circle.logInfo(true);
+
+
+
+
 })();
