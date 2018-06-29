2c50f5bd1affbaa81974db99b5fdc1f225b966f3
Ruusa Bolton <ruusabolton@Ruusas-MacBook-Pro.local>
2018-06-29 11:25:57 -0500

    adding jquery_exercises

---
+++ js/jquery_exercises.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    alert( 'The DOM has finished loading!' );
+
+
+    $(function () {
+        $("#playButton").click(function () {
+            $("#playOutput").text($("#playInput").val())});
+
+
+
+
+        $(".importante").mouseover(function () {
+
+            $('.importante').css('background-color', 'pink');});
+
+
+
+
+
+
+
+
+        $(".codeup").mouseover(function () {
+
+            $('.codeup').css('background-color', 'lightgrey');});
+
+
+    });
+
+    $(".codeup").mouseover(function () {
+
+
+        $(alert( '... loading Animals List!!' ));
+
+    });
+
+});
