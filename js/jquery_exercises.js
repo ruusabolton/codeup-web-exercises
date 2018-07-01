c3bcbd7ef37ec65394f4931d99d77111f180a7b8
Ruusa Bolton <ruusabolton@Ruusas-MacBook-Pro.local>
2018-07-01 17:11:38 -0500

    adding Konami Jquery exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,23 +4,48 @@ $(document).ready(function() {
         $(this).css('background-color', 'red');
     });
 
+
+//'this' will only affect the one one paragraph
+    //on which you doubleclicked. if you want to affect all the paragraphs, replace 'this'
+    //'p'.
     $('p').dblclick(function (e) {
-        $(this).css('font-size', '18px');
+        $('p').css('font-size', '18px');
     });
 
-    // $('li').hover(function() {
-    //
-    //     $(this).css('color', 'green');
-    //
+//will highlight individual li when hovering
+
+    // $('.unselected').hover(function(){
+    //     $(this).addClass('red');
+    // }).mouseout(function(){
+    //     $(this).removeClass('red');
     // });
-    //
-    // $(this).css('color', '');
+
+ //diff approach: function within a function
+
+     $('li').hover(function() {
+         $('li').css('color', 'red');
+     }, function(){
+         $('li').css('color', 'black');
+
+     });
 
 
-    $('.unselected').hover(function(){
-        $(this).addClass('red');
-    }).mouseout(function(){
-        $(this).removeClass('red');
-    });
+    $('#main-header').click(function(){
+    var currentColor = $(this).css('background-color');
+    console.log(currentColor);
+
+    if (currentColor == 'rgb(0, 255,255)'){
+        $(this).css('background-color', 'white');}
+        else {
+            $(this).css('background-color', 'blue');
+            }
+        });
+
+
+
+
+
+
+
 
 });
---
+++ js/konami.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+// keycodes of allowed keys
+var legitKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b',
+    13: 'enter'
+};
+
+// accepted winning sequence
+var stepCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a','enter'];
+
+// a variable to remember the 'position' the user has reached so far.
+var stepCodePosition = 0;
+
+// adding event listener based on key pressed
+document.addEventListener('keydown', function(e) {
+    // check if key is an allowed key
+    var key = legitKeys[event.keyCode];
+    // what is the index position of key pressed (in the sequence)
+    var requiredKey = stepCode[stepCodePosition];
+
+    // if key is allowed key ; is it in the right position of the sequence?
+    if (key == requiredKey) {
+
+        // if True, move on to next pressed key
+        stepCodePosition++;
+        $("clm1").css("display", "block")
+
+        // if the last key is reached, trigger successPopup function
+        if (stepCodePosition == stepCode.length) {
+            successPopup();
+            stepCodePosition = 0;//and reset for next iteration
+        }
+
+       //If key is not allowed or out of sequence, reset to 0
+        }
+
+    else {
+        stepCodePosition = 0;
+        //On screen print alert of Failure
+        $(".statusAlert").html("WRONG stone!! Get steppin'!");
+        $(".statusAlert, #startOverbtn").css("display", "block");
+
+        $("#startOverbtn").click(function() {
+            $(".statusAlert, #startOverbtn").hide();//hide status & button
+        });
+
+    }
+});
+
+
+
+function successPopup() {
+
+    alert("YOU DID IT ....You have added 30 lives!");
+    document.location = 'konami2.html';
+
+
+}
+
+
+
+
+});
+
+
